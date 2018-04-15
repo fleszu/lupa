1e97dd5c95c8a98cd7f2f08687c2c61acfc4b0b7
AdamBabinicz <puaro@vp.pl>
2018-04-15 19:54:57 +0200

    while loops

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-var myAge = 26;
-
-if (myAge < 18 || myAge > 30 || myAge === 25) {
-	document.write("You can come, you cool dude");
-} else {
-	document.write("You aint coming!");
+var age = 5;
+while (age < 10) {
+	console.log("You age is less than 10");
+	age++;
 }
+document.write("You are now over 10");
