88631a7ec443166586ca132d464f8e066aaa3c85
AdamBabinicz <puaro@vp.pl>
2018-04-18 19:14:00 +0200

    zdarzenia

---
+++ list.js
@@ -0,0 +1,8 @@
+var list = document.createElement("ul");
+var wrapper = document.getElementById("wrapper");
+wrapper.appendChild(list);
+
+var firstElement = document.createElement("li");
+list.appendChild(firstElement);
+
+firstElement.innerHTML = "Paryż";
